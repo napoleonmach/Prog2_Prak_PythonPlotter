f3ea09fa3ec9578cf6fe79b0c8930ff231ef4b59
emanu <emanu@DESKTOP-2LJHKAI>
2023-10-16 20:26:27 +0200

    Add new Python-Code

--- Prog2_Prak_PythonPlotter.py
+++ Prog2_Prak_PythonPlotter.py
@@ -18,7 +18,10 @@ def plot_function(expression, x_min, x_max, width, height):
             if row*(1/height)>value and (row-1)*(1/height)<value:
                 line+= "*"
             else:
-                line+= " "    
+                if True: #TODO: Bedingung der Nulllinie einfügen
+                    line+= " "    
+                else:
+                    line+= "_"
         print(line)
 
 
@@ -26,12 +29,12 @@ def plot_function(expression, x_min, x_max, width, height):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Funktion visualisieren")
-    parser.add_argument("expression", type=str, help="Funktionsausdruck, z.B., '2*math.sin(x)+1'")
-    parser.add_argument("x_min", type=float, help="Minimum des Intervalls")
-    parser.add_argument("x_max", type=float, help="Maximum des Intervalls")
-    parser.add_argument("width", type=int, help="Breite der Darstellung")
-    parser.add_argument("height", type=int, help="Hoehe der Darstellung")
+    parser = argparse.ArgumentParser(description="Matheplotter aus Hogwards")
+    parser.add_argument("expression", type=str, help="Funktionsgleichung, z.B., '2*math.sin(x)+1'")
+    parser.add_argument("x_min", type=float, help="Minwerte")
+    parser.add_argument("x_max", type=float, help="Maxwerte")
+    parser.add_argument("width", type=int, help="Darstellungsbreite")
+    parser.add_argument("height", type=int, help="Darstellungshoehe")
     args = parser.parse_args()
 
     plot_function(args.expression, args.x_min, args.x_max, args.width, args.height)
